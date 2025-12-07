19a46ab07e6c92c4490154f4877033294a90bfe0
hweij <henk000@gmail.com>
2025-12-07 12:18:32 +0100

    add blocks for testing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 
 import { createScene } from './scene';
+import { randInt } from 'three/src/math/MathUtils.js';
 
 const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
 const material = new THREE.MeshStandardMaterial({ color: "#ffffff" });
@@ -13,9 +14,9 @@ function createBlock() {
 const scene = createScene();
 
 // Add some simple blocks for testing
-const block0 = createBlock();
-const block1 = createBlock();
-block1.position.x = 0.1;
-const block2 = createBlock();
-block2.position.x = 0.2;
-
+for (let i = 0; i < 100; i++) {
+    const block = createBlock();
+    block.position.x = randInt(-5, 5) * 0.1;
+    block.position.y = randInt(-5, 5) * 0.1;
+    block.position.z = randInt(-5, 5) * 0.1;
+}
