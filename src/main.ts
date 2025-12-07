465d9914d7d732fe2b48bbd5081787169453cfbe
hweij <henk000@gmail.com>
2025-12-07 12:02:18 +0100

    add blocks for testing

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,20 @@ import * as THREE from 'three';
 
 import { createScene } from './scene';
 
-const scene = createScene();
-
-// Add a simple shape for testing
 const geometry = new THREE.BoxGeometry(0.1, 0.1, 0.1);
 const material = new THREE.MeshStandardMaterial({ color: "#ffffff" });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+function createBlock() {
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+    return mesh;
+}
+
+const scene = createScene();
+
+// Add some simple blocks for testing
+const block0 = createBlock();
+const block1 = createBlock();
+block1.position.x = 0.1;
+const block2 = createBlock();
+block2.position.x = 0.2;
+
